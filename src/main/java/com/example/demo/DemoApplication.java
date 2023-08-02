a91cbdb4e0a206e18c298ee5a938f8ae03faeabf
Yuyu Liu <84312974+coyl7@users.noreply.github.com>
2023-08-02 09:50:41 +0100

    Include image and text

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
-public class DwightGifApp {
+public class IdentityTheftApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Dwight Schrute GIF");
+        JFrame frame = new JFrame("Identity Theft Is Not A Joke!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(800, 600);
         frame.setLayout(new BorderLayout());
 
         JLabel textLabel = new JLabel("Identity Theft Is Not A Joke!");
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 24));
         textLabel.setHorizontalAlignment(SwingConstants.CENTER);
         frame.add(textLabel, BorderLayout.NORTH);
 
-        ImageIcon dwightGifIcon = new ImageIcon("https://media.tenor.com/R8Ot9SBUktQAAAAC/dwight-schrute-the-office.gif");
-        JLabel gifLabel = new JLabel(dwightGifIcon);
-        gifLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        frame.add(gifLabel, BorderLayout.CENTER);
+        try {
+            URL imageUrl = new URL("https://i.ytimg.com/vi/25n1rfB3F20/maxresdefault.jpg");
+            Image image = ImageIO.read(imageUrl);
+            ImageIcon imageIcon = new ImageIcon(image);
+            JLabel imageLabel = new JLabel(imageIcon);
+            imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            frame.add(imageLabel, BorderLayout.CENTER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         frame.setVisible(true);
     }
